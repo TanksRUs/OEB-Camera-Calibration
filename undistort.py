1865e1d189f99f368c8f0fb32e5af56ea3edebf6
Duanr <richard.duan@nrc-cnrc.gc.ca>
2022-10-06 11:57:55 -0230

    new branch for undistorting and stitching images

--- undistort.py
+++ undistort.py
@@ -1,18 +1,38 @@
 import cv2 as cv
 import numpy as np
+import csv
 from tkinter import filedialog
 
-# -----COPY & PASTED FROM CSV-----
-mtx = [[856.2565198058854,0.0,523.5020254748135],
-[0.0,859.731351927642,373.8238948932486],
-[0.0,0.0,1.0]]
-mtx = np.array(mtx)
+# -----READ CSV-----
+# csv_path = filedialog.askopenfilename(initialdir='C:/Users/duanr/Desktop/Stitching/')
+NUM_CAMS = 4
+csv_path = 'C:/Users/duanr/Desktop/Stitching/calibration.csv'
+mtxs = []
+dists = []
 
-dist = [[-0.2534300800817647,0.20220112243912391,-0.000777285572553943,-0.00034570247103487444,-0.1215798893894797]]
-dist = np.array(dist)
+with open(csv_path, newline='') as csv_file:
+    reader = csv.reader(csv_file)
+    for i in range(0, NUM_CAMS):
+        for header in range (0, 4): # skips header rows
+            next(reader)
+        mtx = []
+        mtx.append([float(x) for x in next(reader)])
+        mtx.append([float(x) for x in next(reader)])
+        mtx.append([float(x) for x in next(reader)])
+        mtx = np.array(mtx)
+        mtxs.append(mtx)
+        next(reader)
+        next(reader)
+        dist = [[float(x) for x in next(reader)]]
+        dist = np.array(dist)
+        dists.append(dist)
+        try: # in case there's no line return at the end of the file
+            next(reader)
+        except StopIteration:
+            pass
 # --------------------------------
 
-img_path = filedialog.askopenfilename(initialdir='C:/Users/duanr/Desktop/Camera Calibration/Raw Images/')
+folder_path = filedialog.askdirectory(initialdir='C:/Users/duanr/Desktop/Stitching/')
 img = cv.imread(img_path)
 h, w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
